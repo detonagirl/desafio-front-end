8a3e31b242ac2147d797b04e79a66595f87951d6
Mariana Carvalho <contatomarilop@gmail.com>
2020-11-20 11:19:16 -0300

    alterações de estilo, alterações no funcionamento dos botoes e criaçao do contador

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+var contador = 0
+campo.value = contador
+
+menos.addEventListener('click', () => {
+
+    if (contador > 0) {
+        contador-=1
+    }
+    campo.value = contador
+})
+mais.addEventListener('click', () => {
+    contador+=1
+    campo.value = contador
+})
 
